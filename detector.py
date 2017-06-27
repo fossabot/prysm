dcadeacc321f205f86e00f6f744c0e3a6c824c6f
Brandon Dube <brandondube@gmail.com>
2017-06-26 19:40:10 -0700

    WIP detector

---
+++ detector.py
@@ -0,0 +1,53 @@
+'''
+Basic detector interface
+'''
+import numpy as np
+from pillow import PIL
+
+px_size_default = 5                    # um
+resolution_default = (1024,1024)       # px * px
+noise_default = dict(read=5,           # e-
+    bias=200,                          # DN
+    dark=0.2,                          # e-/s
+    prnu=np.zeros(resolution_default), # e- per pixel
+    dsnu=np.zeros(resolution_default)  # e- per pixel
+)
+wavelengths_default = np.arange(400, 700, 10)    # nm
+qe_default = (wavelengths_default-700)/700 + 0.5 # e-/photon
+light_default = dict(wavelengths=wavelengths_default,
+    qe=qe_default,
+    fwc=50000 # e-
+)
+
+class Detector(object):
+    def __init__(self, pixel_size=px_size_default, resolution=resolution_default, noise=noise_default, light=light_default):
+        self.pixel_size = pixel_size
+        self.resolution = resolution
+        self.noise = noise
+        self.light = light
+
+    def expose(self, truth_img, truth_wavelengths, ts=1/100):
+        # start from nothing
+        output_image = np.zeros((self.resolution, self.resolution))
+
+        # add the bias
+        output_image += self.noise['bias']
+
+        # add the dark current
+        output_image += (self.noise['dark']*ts) + self.noise['dsnu']
+
+        # add the read noise
+        output_image += np.random.rand((self.resolution, self.resolution)) * self.noise['read']
+
+        # add the signal
+        spectral_content = np.dot(self.light['qe'], truth_wavelengths[:,:,2])
+        output_image += truth_img * spectral_content * ts
+        
+        return output_image
+
+
+
+class ADC(object):
+    def __init(self, precision=16, noise=5):
+        self.precision = precision
+        self.noise = noise
