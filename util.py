8b4d9072b845937061a82f76f26751741a77bbcb
Brandon Dube <brandondube@gmail.com>
2017-06-26 19:35:07 -0700

    general deps

---
+++ util.py
@@ -0,0 +1,5 @@
+def pupil_sample_to_psf_sample(pupil_sample, num_samples, wavelength, efl):
+    return (wavelength * efl * 1e3) / (pupil_sample * num_samples)
+
+def psf_sample_to_pupil_sample(psf_sample, num_samples, wavelength, efl):
+    return (psf_sample * num_samples) / (wavelength * efl * 1e3)
