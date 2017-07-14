df81766f88ddc17d7c4705c81a4c9a23bae52448
Brandon Dube <brandondube@gmail.com>
2017-07-13 23:04:41 -0700

    cut dead code, plot_fourier_chain

--- code6/__init__.py
+++ code6/__init__.py
@@ -1,18 +1,20 @@
-from code6.core import Engine
+from code6.core import convolve_pixel, plot_fourier_chain
 from code6.detector import Detector
 from code6.fringezernike import FringeZernike
 from code6.lens import Lens
 from code6.otf import MTF
 from code6.psf import PSF
 from code6.pupil import Pupil
-from code6.simulation import Simulation
+from code6.seidel import Seidel
 
-__all__ = ['Engine',
-           'Detector',
-           'FringeZernike',
-           'Lens',
-           'MTF',
-           'PSF',
-           'Pupil',
-           'Simulation',
+__all__ = [
+    'plot_fourier_chain',
+    'convolve_pixel',
+    'Detector',
+    'Lens',
+    'Pupil',
+    'FringeZernike',
+    'Seidel',
+    'PSF',
+    'MTF',
 ]
--- code6/core.py
+++ code6/core.py
@@ -4,74 +4,35 @@ Base meta-class for aberration simulations that provides wavefront, PSF, and MTF
 import numpy as np
 from numpy import arctan2, exp, floor, nan, sqrt, pi
 from numpy import power as npow
-from numpy.fft import fft2, fftshift, ifftshift
+from numpy.fft import fft2, ifft2, fftshift, ifftshift
 from matplotlib import pyplot as plt
 
 from code6.lens import Lens
 from code6.fringezernike import FringeZernike
-from code6.simulation import Simulation
 from code6.psf import PSF
 from code6.otf import MTF
 from code6.fttools import pad2d
-from code6.util import pupil_sample_to_psf_sample, psf_sample_to_pupil_sample
+from code6.util import pupil_sample_to_psf_sample, psf_sample_to_pupil_sample, share_fig_ax
 
-class Engine(object):
-    def __init__(self, parameters=Simulation(), lens=Lens(), pupil=FringeZernike()):
-        epd = lens.efl / lens.fno
-        rho = epd / 2
-        psf_samples = (parameters.padding * parameters.samples) + parameters.samples
+def plot_fourier_chain(pupil, psf, mtf, fig=None, axs=None):
 
-        self.parameters = parameters
-        self.lens = lens
-        self.pupil = pupil
+    # create a figure
+    fig, axs = share_fig_ax(fig, axs)
 
-        # and bools indicating if things are computed
-        self._pupil_computed = False
-        self._psf_computed = False
-        self._mtf_computed = False
+    pupil.plot2d(fig=fig, ax=axs[0])
+    psf.plot2d(fig=fig, ax=axs[1])
+    mtf.plot2d(fig=fig, ax=axs[2])
 
-    # helper methods -----------------------------------------------------------
+    fig.tight_layout()
+    return fig, axs
 
-    def _check_pupil(self):
-        if not self._pupil_computed:
-            # self.pupil_phase, self.pupil_fcn = 
-            self.pupil.build(wavelength=self.parameters.wavelength)
-            self.pupil.clip()
-            self._pupil_computed = True
-
-    def _check_psf(self):
-        if not self._psf_computed:
-            self._check_pupil()
-            self.psf = PSF.from_pupil(self.pupil,
-                                      self.parameters.wavelength,
-                                      self.lens.efl,
-                                      self.parameters.padding)
-            self._psf_computed = True
-
-    def _check_mtf(self):
-        if not self._mtf_computed:
-            self._check_psf()
-            self.mtf = MTF.from_psf(self.psf)
-            self._mtf_computed = True
-
-    def plot_fourier_chain(self):
-        self._check_mtf()
-        # generate a figure
-        fig, ax = plt.subplots(nrows=1, ncols=3)
-
-        # glue in the desired plots
-        _, pupil_axis = self.pupil.plot2d()
-        ax[0] = pupil_axis
-
-        _, psf_axis = self.psf.plot2d()
-        ax[1] = psf_axis
-
-        _, mtf_axis = self.mtf.plot_tan_sag()
-        ax[2] = mtf_axis
-        fig.show()
-        return fig, ax
-
-    def __repr__(self):
-        return 'Engine'
-
-    # helper methods -----------------------------------------------------------
+def convolve_pixel(pitch, psf):
+    error = psf.sample_spacing/2
+    print(f'This function does not resample, pixel pitch is enlarged by {error}')
+    half_width = pitch / 2
+    steps = int(np.floor(half_width / psf.sample_spacing))
+    pixel_aperture = np.zeros((psf.samples, psf.samples))
+    pixel_aperture[psf.center-steps:psf.center+steps, psf.center-steps:psf.center+steps] = 1
+    ft_ape = fft2(pixel_aperture)
+    ft_psf = fft2(psf.data)
+    return ifftshift(ifft2(np.absolute(ft_ape * ft_psf)))
