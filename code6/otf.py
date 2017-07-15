0c65de40bc3794bdac16054ee46537739cd67f21
Brandon Dube <brandondube@gmail.com>
2017-07-14 17:07:31 -0700

    MTF from pupil

--- code6/otf.py
+++ code6/otf.py
@@ -7,6 +7,7 @@ from numpy.fft import fft2, fftshift, ifftshift
 
 from matplotlib import pyplot as plt
 
+from code6.psf import PSF
 from code6.util import correct_gamma, share_fig_ax
 
 class MTF(object):
@@ -85,6 +86,11 @@ class MTF(object):
         f_s = int(floor(psf.samples / 2))
         unit = 1 / (psf.sample_spacing / 1e3) * range(-f_s, f_s) / psf.samples
         return MTF(dat/dat[f_s,f_s], unit)
+
+    @staticmethod
+    def from_pupil(pupil, efl):
+        psf = PSF.from_pupil(pupil, efl=efl)
+        return __class__.from_psf(psf)
 
 def diffraction_limited_mtf(fno=1, wavelength=0.5, num_pts=128):
     '''
