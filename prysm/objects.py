e9fc027244af16961bf161b5bbee251d4517a7b2
Brandon Dube <brandondube@gmail.com>
2017-08-18 22:28:36 -0700

    RGB images

--- prysm/objects.py
+++ prysm/objects.py
@@ -1,12 +1,16 @@
 ''' Object to convolve lens PSFs with
 '''
+from multiprocessing.dummy import Pool as ThreadPool
+from functools import lru_cache
+
 import numpy as np
 from numpy.fft import fft2, ifft2, fftshift, ifftshift
 
-from scipy.misc import imsave
+from scipy.misc import imsave, imread
 
 from matplotlib import pyplot as plt
 
+from prysm.conf import config
 from prysm.coordinates import cart_to_polar
 from prysm.psf import PSF, _unequal_spacing_conv_core
 from prysm.fttools import forward_ft_unit
@@ -15,13 +19,13 @@ from prysm.util import correct_gamma, share_fig_ax
 class Image(object):
     ''' Images of an object
     '''
-    def __init__(self, data, sample_spacing):
+    def __init__(self, data, sample_spacing, synthetic=True):
         ''' Creates a new Image object.
 
         Args:
             data (`numpy.ndarray`): data that represents the image, 2D.
 
-            sample-spacing (`float`): pixel pitch of the data.
+            sample_spacing (`float`): pixel pitch of the data.
 
         '''
         self.data = data
@@ -30,6 +34,7 @@ class Image(object):
         self.center_x, self.center_y = self.samples_x // 2, self.samples_y // 2
         self.ext_x = sample_spacing * self.center_x
         self.ext_y = sample_spacing * self.center_y
+        self.synthetic = synthetic
 
     def show(self, interp_method=None, fig=None, ax=None):
         ''' Displays the image.
@@ -49,9 +54,9 @@ class Image(object):
                 `matplotlib.axis`: axis containing the plot.
 
         '''
-        ex, ey = self.ext_x, self.ext_y
         lims = (0,1)
         fig, ax = share_fig_ax(fig, ax)
+
         ax.imshow(self.data,
                   cmap='Greys_r',
                   interpolation=interp_method,
@@ -78,7 +83,8 @@ class Image(object):
         conved_image = _unequal_spacing_conv_core(img_psf, psf)
         #return conved_image
         return Image(data=conved_image.data,
-                     sample_spacing=self.sample_spacing)
+                     sample_spacing=self.sample_spacing,
+                     synthetic=self.synthetic)
 
     def save(self, path, nbits=8):
         ''' Write the image to a png, jpg, tiff, etc.
@@ -93,12 +99,202 @@ class Image(object):
 
         '''
         dat = (self.data * 255).astype(np.uint8)
+
+        if self.synthetic is False:
+            # was a real image, need to flip vertically.
+            dat = np.flip(dat, axis=0)
         imsave(path, dat)
 
+    @staticmethod
+    def from_file(path, scale):
+        ''' Reads a file into a new Image instance, always monochrome
+
+        Args:
+            path (`string`): path to a file.
+
+            scale (`float`): pixel scale, in microns.
+
+        Returns:
+            `Image`: a new image object.
+
+        Notes:
+            TODO: proper handling of images with more than 8bpp.
+        '''
+        imgarr = imread(path, flatten=True, mode='F')
+
+        return Image(data=np.flip(imgarr, axis=0)/255, sample_spacing=scale, synthetic=False)
+
+class RGBImage(object):
+    ''' RGB images
+    '''
+    def __init__(self, r, g, b, sample_spacing, synthetic=True):
+        ''' creates a new RGB image
+        
+        Args:
+            r (`Image`): array for the red channel.
+
+            g (`Image`): array for the green channel.
+
+            b (`Image`): array for the blue channel.
+
+            sample_spacing (`float`): spacing between samples, in microns.
+
+            synthetic (`bool`): whether or not the image is synthetic.  Real
+                images are upside-down compared to synthetic ones.
+
+        Returns:
+            `RGBImage`, new RGB image.
+        '''
+        # load in data
+        self.R = r
+        self.G = g
+        self.B = b
+
+        # load in sampling information
+        self.sample_spacing = sample_spacing
+        self.samples_x, self.samples_y = r.shape
+        self.center_x, self.center_y = self.samples_x // 2, self.samples_y // 2
+        self.ext_x = sample_spacing * self.center_x
+        self.ext_y = sample_spacing * self.center_y
+
+        # load in synthetic or not
+        self.synthetic = synthetic
+
+    def show(self, interp_method=None, fig=None, ax=None):
+        ''' Displays the image.
+
+        Args:
+            interp_method (`string`): interpolation technique used in display.
+
+            fig (`matplotlib.figure`): figure to display in.
+
+            ax (`matplotlib.axis`): axis to display in.
+
+        Returns:
+            `tuple` containing:
+
+                `matplotlib.figure`: figure containing the plot.
+
+                `matplotlib.axis`: axis containing the plot.
+
+        '''
+        ex, ey = self.ext_x, self.ext_y
+        lims = (0,1)
+        fig, ax = share_fig_ax(fig, ax)
+
+        dat = rgbimage_to_datacube(self)
+        if self.synthetic is not True:
+            dat = np.flip(dat, axis=0)
+        ax.imshow(dat,
+                  interpolation=interp_method,
+                  origin='lower')
+        return fig, ax
+
+    def as_psf(self, color='g'):
+        ''' Converts a color plane of this image to a PSF object.
+
+        Args:
+            color (`string`): red, green, or blue
+
+        Returns:
+            `PSF`: a PSF object for the given color plane.
+
+        '''
+        if color.lower() in ('g', 'green'):
+            dat = self.G
+        elif color.lower() in ('r', 'red'):
+            dat = self.R
+        elif color.lower() in ('b', 'blue'):
+            dat = self.B
+        else:
+            raise ValueError('invalid color selected')
+
+        return PSF(dat, self.samples_x, self.sample_spacing)
+
+    def save(self, path, nbits=8):
+        ''' Write the image to a png, jpg, tiff, etc.
+
+        Args:
+            path (`string`): path to write the image to.
+
+            nbits (`int`): number of bits in the output image.
+
+        Returns:
+            null: no return
+
+        '''
+        dat = rgbimage_to_datacube(self)
+
+        if self.synthetic is not True:
+            # was a real image, need to flip vertically.
+            dat = np.flip(dat, axis=0)
+            pass
+
+        imsave(path, dat)
+
+    def convpsf(self, rgbpsf):
+        ''' Convolves with a PSF for image simulation
+
+        Args:
+            rgbpsf (`RGBPSF`): an RGBPSF
+
+        Returns:
+            `RGBImage`: A new, blurred image.
+
+        '''
+        psfr = self.as_psf('r')
+        psfg = self.as_psf('g')
+        psfb = self.as_psf('b')
+
+        psfb.plot2d()
+        rconv = _unequal_spacing_conv_core(psfr, rgbpsf.r_psf)
+        gconv = _unequal_spacing_conv_core(psfg, rgbpsf.g_psf)
+        bconv = _unequal_spacing_conv_core(psfb, rgbpsf.b_psf)
+        return RGBImage(r=rconv.data, g=gconv.data, b=bconv.data,
+                        sample_spacing=self.sample_spacing,
+                        synthetic=self.synthetic)
+
+    @staticmethod
+    def from_file(path, scale):
+        ''' Reads a file into a new RGBImage instance, must be 24bpp/8bpc
+
+        Args:
+            path (`string`): path to a file.
+
+            scale (`float`): pixel scale, in microns.
+
+        Returns:
+            `RGBImage`: a new image object.
+
+        Notes:
+            TODO: proper handling of images with more than 8bpp.
+        '''
+        # img is an mxnx3 array of unit8s
+        img = imread(path).astype(config.precision()) / 255
+
+        img = np.flip(img, axis=0)
+        return RGBImage(r=img[:,:,0], g=img[:,:,1], b=img[:,:,2],
+                        sample_spacing=scale, synthetic=False)
+
+def rgbimage_to_datacube(rgbimage):
+    ''' Creates an mxnx3 array from an RGBImage
+
+    Args:
+        rgbimage (`RGBImage`): an RGBImage object.
+
+    Returns:
+        `numpy.ndarray`: an ndarray of shape m x n x 3.
+    '''
+    dat = np.empty((rgbimage.samples_x, rgbimage.samples_y, 3), dtype=np.uint8)
+    dat[:,:,0] = rgbimage.R * 255
+    dat[:,:,1] = rgbimage.G * 255
+    dat[:,:,2] = rgbimage.B * 255
+    return dat
 
 class Slit(Image):
     ''' Representation of a single slit.
     '''
+    @lru_cache()
     def __init__(self, width, orientation='Vertical', sample_spacing=0.075, samples=384):
         ''' Creates a new Slit instance.
 
@@ -139,6 +335,7 @@ class Slit(Image):
 class Pinhole(Image):
     ''' Representation of a pinhole object.
     '''
+    @lru_cache()
     def __init__(self, width, sample_spacing=0.025, samples=384):
         ''' Produces a Pinhole.
 
@@ -166,6 +363,7 @@ class Pinhole(Image):
 class SiemensStar(Image):
     ''' Representation of a Siemen's star object.
     '''
+    @lru_cache()
     def __init__(self, num_spokes, sinusoidal=True, sample_spacing=2, samples=384):
         ''' Produces a Siemen's Star.
 
