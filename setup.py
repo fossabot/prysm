3c49963637c667628f67ff21eb6c029f62b7b0f4
Brandon Dube <brandondube@gmail.com>
2017-08-21 12:57:08 -0400

    update distribution info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='prysm',
-    version='0.1.0',
+    version='0.2.0',
     description='A python optics module',
     long_description='Uses geometric and fourier optics to model optical systems',
     license='MIT',
