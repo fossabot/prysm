e9b6726b0110ef74d997cbc803b7bb9afe030821
Brandon Dube <brandondube@gmail.com>
2017-11-22 13:32:23 -0500

    implement ocean optics spectrometer file reader and spectrum object

---
+++ prysm/io.py
@@ -0,0 +1,34 @@
+''' File readers (and someday, writers) for various commercial instruments
+'''
+import re
+
+from prysm.spectrum import Spectrum
+
+def read_oceanoptics(file_path):
+    ''' Reads spectral transmission data from an ocean optics spectrometer
+        into a new Spectrum object.
+
+    Args:
+        file_path (`string`): path to a file.
+
+    Returns:
+        `Spectrum`: a new Spectrum object.
+
+    '''
+    with open(file_path, 'r') as fid:
+        # txt = fid.read()
+        txtlines = fid.readlines()
+
+        wavelengths, values = [], []
+        idx = None
+        for i, line in enumerate(txtlines):
+            if '>>>>>Begin Spectral Data<<<<<' in line:
+                idx = i
+
+        data_lines = txtlines[idx+1:]
+        for line in data_lines:
+            wvl, v = line.split()
+            wavelengths.append(float(wvl))
+            values.append(float(v))
+
+        return Spectrum(wavelengths=wavelengths, values=values)
