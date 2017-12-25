cd29d75e268968215a1a94f1e92e33bebe6a64f4
Brandon Dube <brandondube@gmail.com>
2017-12-25 12:31:38 -0500

    parameterize psf test, +mtf test

--- tests/test_physics.py
+++ tests/test_physics.py
@@ -2,22 +2,43 @@
 '''
 import numpy as np
 
-from prysm import Pupil, PSF
+import pytest
+
+from prysm import Pupil, PSF, MTF
 from prysm.psf import airydisk
+from prysm.otf import diffraction_limited_mtf
 
 PRECISION = 1e-3  # ~0.1%
 
+TEST_PARAMETERS = [
+    (10.0, 1.000, 0.5),  # f/10, visible light
+    (10.0, 1.000, 1.0),  # f/10, SWIR light
+    (3.00, 1.125, 3.0)]  # f/2.66666, MWIR light
 
-def test_diffprop_matches_airydisk():
-    epd = 1
-    efl = 10
+
+@pytest.mark.parameterize('efl, epd, wvl', TEST_PARAMETERS)
+def test_diffprop_matches_airydisk(efl, epd, wvl):
     fno = efl / epd
-    wavelength = 0.5
 
-    p = Pupil(wavelength=wavelength, epd=epd)
+    p = Pupil(wavelength=wvl, epd=epd)
     psf = PSF.from_pupil(p, efl)
     u, sx = psf.slice_x
     u, sy = psf.slice_y
-    analytic = airydisk(u, fno, wavelength)
-    assert np.allclose(sx, analytic, rtol=1e-3, atol=1e-3)
-    assert np.allclose(sy, analytic, rtol=1e-3, atol=1e-3)
+    analytic = airydisk(u, fno, wvl)
+    assert np.allclose(sx, analytic, rtol=PRECISION, atol=PRECISION)
+    assert np.allclose(sy, analytic, rtol=PRECISION, atol=PRECISION)
+
+
+@pytest.mark.parameterize('efl, epd, wvl', TEST_PARAMETERS)
+def test_diffprop_matches_analyticmtf(efl, epd, wvl):
+    fno = efl / epd
+    p = Pupil(wavelength=wvl, epd=epd)
+    psf = PSF.from_pupil(p, efl)
+    mtf = MTF.from_psf(psf)
+    u, t = mtf.tan
+    uu, s = mtf.sag
+
+    analytic_1 = diffraction_limited_mtf(fno, wvl, frequencies=u)
+    analytic_2 = diffraction_limited_mtf(fno, wvl, frequencies=uu)
+    assert np.allclose(analytic_1, t, rtol=PRECISION, atol=PRECISION)
+    assert np.allclose(analytic_2, s, rtol=PRECISION, atol=PRECISION)
