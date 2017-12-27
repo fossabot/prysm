05db22ebf1ee1b3ed202148b293eb55472e325c5
Brandon Dube <brandondube@gmail.com>
2017-12-26 19:27:36 -0500

    + read_oceanoptics test

---
+++ tests/test_io.py
@@ -0,0 +1,26 @@
+''' Tests the io functions of prysm.
+'''
+from pathlib import Path
+
+from prysm.io import read_oceanoptics
+
+
+def test_read_oceanoptics_functions():
+    # file, has 2048 pixels
+    p = Path(__file__).parent / 'io_files' / 'sample_oceanoptics.txt'
+    data = read_oceanoptics(p)
+
+    # returns dict with wvl, value keys
+    assert 'wvl' in data
+    assert 'values' in data
+
+    # data is of the proper length
+    assert len(data['wvl'] == 2048)
+    assert len(data['values'] == 2048)
+
+    # data begins and ends with correct values
+    assert data['wvl'][0] == 178.179
+    assert data['values'][0] == 556.52
+
+    assert data['wvl'][-1] == 871.906
+    assert data['values'][-1] == 84.35
