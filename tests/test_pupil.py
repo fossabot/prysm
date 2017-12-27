d8aa877ecbf0990e183edcbcff4cdc9727542c87
Brandon Dube <brandondube@gmail.com>
2017-12-26 20:52:49 -0500

    + bad OPD unit test on pupils

--- tests/test_pupil.py
+++ tests/test_pupil.py
@@ -41,23 +41,28 @@ def test_pupil_passes_valid_params():
         'wavelength': 0.6328,
         'opd_unit': 'nm'}
     p = Pupil(**parameters)
-    assert(p.samples == parameters['samples'])
-    assert(p.epd == parameters['epd'])
-    assert(p.wavelength == parameters['wavelength'])
-    assert(p._opd_str == parameters['opd_unit'])
-    assert(p._opd_unit == 'nanometers')  # make sure this is updated if the test is changed to a different unit
+    assert p.samples == parameters['samples']
+    assert p.epd == parameters['epd']
+    assert p.wavelength == parameters['wavelength']
+    assert p._opd_str == parameters['opd_unit']
+    assert p._opd_unit == 'nanometers'  # make sure this is updated if the test is changed to a different unit
+
+
+def test_pupil_rejects_bad_opd_unit():
+    with pytest.raises(ValueError):
+        Pupil(opd_unit='foo')
 
 
 def test_pupil_has_zero_pv(p):
-    assert(p.pv == pytest.approx(0))
+    assert p.pv == pytest.approx(0)
 
 
 def test_pupil_has_zero_rms(p):
-    assert(p.rms == pytest.approx(0))
+    assert p.rms == pytest.approx(0)
 
 
 def test_tilt_pupil_axis_is_not_x(p_tlt):
     u, x = p_tlt.slice_x
     idxs = np.isfinite(x)
     zeros = np.zeros(x.shape)
-    assert(np.allclose(x[idxs], zeros[idxs]))
+    assert np.allclose(x[idxs], zeros[idxs])
